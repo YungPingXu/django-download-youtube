4a51bee39d0b5828e410a27c9a76580b6ed5844c
YungPingXu <a0976373843@gmail.com>
2020-02-27 04:49:07 +0800

    commit

--- clock.py
+++ clock.py
@@ -7,10 +7,10 @@ import os
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=50)
+@sched.scheduled_job('interval', minutes=32)
 def timed_job_awake_your_app():
-    print('awake app every 50 minutes.')
-    url = 'https://online-download-youtube.herokuapp.com'
+    print('awake app every 32 minutes.')
+    url = 'https://online-download-youtube.herokuapp.com/'
     requests.get(url)
     """shutil.rmtree('mysite/media')
     os.mkdir('mysite/media')
