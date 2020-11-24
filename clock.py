d250db4bdf762a4b2de48361c20f48e61a728ad2
許永平 <52243909+YungPingXu@users.noreply.github.com>
2020-11-24 19:35:35 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -7,7 +7,7 @@ import os
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=29)
+@sched.scheduled_job('interval', minutes=25)
 def timed_job_awake_your_app():
     print('awake app every 2 minutes.')
     url = 'https://online-download-youtube.herokuapp.com/'
@@ -19,4 +19,4 @@ def timed_job_awake_your_app():
     if not os.path.isdir('mysite/media/message'):
         os.mkdir('mysite/media/message')
 
-sched.start() 
+sched.start()
